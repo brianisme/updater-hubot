ed584c4f320227ab4de9c16b82d47789a292eb4b
Updater <updater@Brians-Mac-mini-2.local>
2014-10-31 20:49:41 -0400

    trying get around the permission issue when get on/off airplay...

--- scripts/airplay.sh
+++ scripts/airplay.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 osascript -e 'tell application "System Preferences"' -e 'reveal anchor "output" of pane id "com.apple.preference.sound"' -e 'activate' -e 'tell application "System Events"' -e 'tell process "System Preferences"' -e 'select (row 1 of table 1 of scroll area 1 of tab group 1 of window "Sound" whose value of text field 1 is "$1")' -e 'end tell' -e 'end tell' -e 'quit' -e 'end tell'
+
---
+++ scripts/sudoer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+osascript -e 'do shell script "./airplay.sh $3" user name "$1" password "$2" with administrator privileges'
